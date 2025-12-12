e138ffd27c009450144f71abcb1de6f938b56d98
genjiboy0312 <zxo9988@naver.com>
2025-12-12 17:39:49 +0900

    Commit : 공부내용 추가

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -21,6 +21,54 @@
 // Draw(); //현재 처리중인 데이터(=삼각형 1)를 화면에 그림
 // Activate(t2); //삼각형 2를 처리중인 상태로 설정
 // Draw(); //현재 처리중인 데이터(=삼각형 2)를 화면에 그림
+//--------Shader 컴파일 함수----------//
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type); //셰이더 객체 생성(마찬가지)
+	const char* src = source.c_str();
+	glShaderSource(id, // 셰이더의 소스 코드 명시, 소스 코드를 명시할 셰이더 객체 id
+		1, // 몇 개의 소스 코드를 명시할 것인지
+		&src, // 실제 소스 코드가 들어있는 문자열의 주소값
+		nullptr); // 해당 문자열 전체를 사용할 경우 nullptr입력, 아니라면 길이 명시
+	glCompileShader(id); // id에 해당하는 셰이더 컴파일
+
+	// Error Handling(없으면 셰이더 프로그래밍할때 괴롭다...)
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result); //셰이더 프로그램으로부터 컴파일 결과(log)를 얻어옴
+	if (result == GL_FALSE) //컴파일에 실패한 경우
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length); //log의 길이를 얻어옴
+		char* message = (char*)alloca(length * sizeof(char)); //stack에 동적할당
+		glGetShaderInfoLog(id, length, &length, message); //길이만큼 log를 얻어옴
+		std::cout << "셰이더 컴파일 실패! " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id); //컴파일 실패한 경우 셰이더 삭제
+		return 0;
+	}
+
+	return id;
+}
+
+//--------Shader 프로그램 생성, 컴파일, 링크----------//
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragShader)
+{
+	unsigned int program = glCreateProgram(); //셰이더 프로그램 객체 생성(int에 저장되는 것은 id)
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragShader);
+
+	//컴파일된 셰이더 코드를 program에 추가하고 링크
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	//셰이더 프로그램을 생성했으므로 vs, fs 개별 프로그램은 더이상 필요 없음
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
 int main(void)
 {
 	GLFWwindow* window;
@@ -50,10 +98,10 @@ int main(void)
 	//-----Modern OpenGL 방식--------//
 	// http://docs.gl/ 에서 아래 gl함수들의 reference를 찾을 수 있음
 
-	float positions[6] = { //삼각형 좌표 정보. 현재는 CPU side memory에 있음
-		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f,
+	float positions[9] = { //삼각형 좌표 정보. 현재는 CPU side memory에 있음
+		-0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f,0.0f,
+		 0.5f, -0.5f,0.0f,
 	};
 
 	//---------데이터를 전달하는 과정--------//
@@ -62,9 +110,41 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, bufferID); //2. 방금 만든 buffer(두 번째 인자로 주소를 넘겨준)를 "작업 상태"로 만듬
 	// 첫 번째 GL_ARRAY_BUFFER는 버퍼에 배열 데이터가 저장될 것이라는 의미
 	glBufferData(GL_ARRAY_BUFFER, //3. 이제 실제로 GPU에 데이터를 넘겨주는 함수를 호출 
-		6 * sizeof(float), //데이터의 크기를 전달
+		9 * sizeof(float), //데이터의 크기를 전달
 		positions,		  //데이터 포인터 전달
 		GL_STATIC_DRAW);   //데이터 변경이 적을 것이라는 것을 알려줌(GPU의 효율적인 동작을 위한 Hint일 뿐)
+
+	//	데이터를 해석하는 방법
+	glEnableVertexAttribArray(0);
+
+	//	3 부분 : 하나의 vertex에 몇 개의 데이터를 넘기는지
+	//	sizeof(float) * 3 : 만큼 건너 뛰라는 것
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
+	//---------Shader 생성---------------//
+	std::string vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;" //여기 있는 location = 0가, 118, 119 line의 0을 의미함
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n" //119에서 보다시피, 2개의 값만 전달했지만, 알아서 vec4로 변환해줌
+		"}\n";
+
+	std::string fragShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;" //출력 color
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0, 0.0 ,0.0, 1.0);\n" //빨간색 반환
+		"}\n";
+
+	unsigned int shader = CreateShader(vertexShader, fragShader);
+	glUseProgram(shader); //BindBuffer와 마찬가지로, 현재 셰이더 프로그램을 "작업 상태"로 놓음
+	//draw call은 작업 상태인 셰이더 프로그램을 사용하여 작업 상태인 버퍼 데이터를 그림
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -97,6 +177,7 @@ int main(void)
 		glfwPollEvents();
 	}
 
+	glDeleteProgram(shader);			//	셰이더 삭제
 	glfwTerminate();
 	return 0;
 }
